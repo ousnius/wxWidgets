ousnius/wxWidgets

commit 96eaa04e9bcccb3d13c190644193a0ddb8eadd4c

Stop scaling icons loaded from resources in wxMSW
This is not backwards compatible but looks like the right thing to do
because old behaviour was inconsistent: using LoadIcon() meant that we
always loaded icons of the given (system default size) which meant that
some icons were upscaled while the others were not, e.g. in a system
using 200% scaling 16x16 icons were loaded as 32x32 ones, but so were
32x32 icons too.

Use LoadImage() to load the image in its actual size, where "actual"
means the size of the first icon for icons with multiple bitmaps. This
might not be the best thing to do and maybe we should load the default
icon size if the icon provides it, which could be done in a way similar
to what we do in MSW-specific wxIconBundle::AddIcon().

Not scaling the icons means that they always use scale of 1, which makes
things behave correctly for DPI-unaware applications that were broken
before, which seems better even if it breaks DPI-aware applications: but
those should be using wxBitmapBundle, which still works correctly.

See #26370.

Closes #26369.

src/msw/gdiimage.cpp
@@ -606,49 +606,42 @@ bool wxICOResourceHandler::LoadIcon(wxIcon *icon,
     HICON hicon;
 
     // do we need the icon of the specific size or would any icon do?
-    bool hasSize = desiredWidth != -1 || desiredHeight != -1;
+    const bool hasSize = desiredWidth != -1 && desiredHeight != -1;
 
-    wxASSERT_MSG( !hasSize || (desiredWidth != -1 && desiredHeight != -1),
-                  wxT("width and height should be either both -1 or not") );
+    if ( !hasSize )
+    {
+        wxASSERT_MSG( desiredWidth == -1 && desiredHeight == -1,
+                      wxT("width and height should be either both -1 or not") );
+
+        // LoadImage() interprets 0 as meaning "use the actual size".
+        desiredWidth =
+        desiredHeight = 0;
+    }
 
     // try to load the icon from this program first to allow overriding the
     // standard icons (although why one would want to do it considering that
     // we already have wxApp::GetStdIcon() is unclear)
+    hicon = (HICON)::LoadImage(wxGetInstance(), name.t_str(), IMAGE_ICON,
+                                desiredWidth, desiredHeight,
+                                LR_DEFAULTCOLOR);
 
-    // note that we can't just always call LoadImage() because it seems to do
-    // some icon rescaling internally which results in very ugly 16x16 icons
-    if ( hasSize )
+    // next check if it's not a standard icon
+    if ( !hicon )
     {
-        hicon = (HICON)::LoadImage(wxGetInstance(), name.t_str(), IMAGE_ICON,
-                                    desiredWidth, desiredHeight,
-                                    LR_DEFAULTCOLOR);
-
-        // Don't give errors when looking for a standard icon because we
-        // provide fallback for them below, but do indicate that we failed to
-        // load other icons because this is probably not expected.
-        if ( !hicon && nStdIcon == WXSIZEOF(stdIcons) )
+        if ( nStdIcon == WXSIZEOF(stdIcons) )
         {
             wxLogLastError(wxString::Format("LoadImage(%s)", name));
         }
-    }
-    else
-    {
-        hicon = ::LoadIcon(wxGetInstance(), name.t_str());
-
-        // As above, only warn for non standard icons.
-        if ( !hicon && nStdIcon == WXSIZEOF(stdIcons) )
+        else // We may still succeed in loading the standard icon.
         {
-            wxLogLastError(wxString::Format("LoadIcon(%s)", name));
-        }
-    }
-
-    // next check if it's not a standard icon
-    if ( !hicon && !hasSize && nStdIcon < WXSIZEOF(stdIcons) )
-    {
-        hicon = ::LoadIcon((HINSTANCE)nullptr, stdIcons[nStdIcon].id);
-        if ( !hicon )
-        {
-            wxLogLastError(wxString::Format("LoadIcon(%s)", stdIcons[nStdIcon].name));
+            const auto& entry = stdIcons[nStdIcon];
+            hicon = (HICON)::LoadImage((HINSTANCE)nullptr, entry.id, IMAGE_ICON,
+                                       desiredWidth, desiredHeight,
+                                       LR_DEFAULTSIZE | LR_DEFAULTCOLOR);
+            if ( !hicon )
+            {
+                wxLogLastError(wxString::Format("LoadImage(%s)", entry.name));
+            }
         }
     }
 
@@ -656,22 +649,17 @@ bool wxICOResourceHandler::LoadIcon(wxIcon *icon,
         return false;
 
     wxSize size;
-    double scale = 1.0;
     if ( hasSize )
     {
         size.x = desiredWidth;
         size.y = desiredHeight;
     }
-    else // We loaded an icon of default size.
+    else
     {
-        // LoadIcon() returns icons of scaled size, so we must use the correct
-        // scaling factor of them.
         size = wxGetHiconSize(hicon);
-        if ( const wxWindow* win = wxApp::GetMainTopWindow() )
-            scale = win->GetDPIScaleFactor();
     }
 
-    return icon->InitFromHICON((WXHICON)hicon, size.x, size.y, scale);
+    return icon->InitFromHICON((WXHICON)hicon, size.x, size.y);
 }
 
 #if wxUSE_PNG_RESOURCE_HANDLER

commit b45b02c22747ad5e4bd9fc5de9f6afa88121531b

Fix loading of standard icons recently broken in wxMSW
The changes of 96eaa04e9bc (Stop scaling icons loaded from resources in
wxMSW, 2026-04-10) broke loading standard icons because they must be
loaded with LR_SHARED flag and LoadImage() simply fails to load them
without it.

But there is no point in using LoadImage() for the standard icons anyhow
as it behaves in exactly the same way as LoadIcon(), so just restore the
code from before the commit above, including setting the scale factor
for the standard icons.

Still preserve the change of that commit for the application-defined
icons, which is what counts.

See #26369, #26370.

src/msw/gdiimage.cpp
@@ -618,6 +618,8 @@ bool wxICOResourceHandler::LoadIcon(wxIcon *icon,
         desiredHeight = 0;
     }
 
+    bool iconMayBeScaled = false;
+
     // try to load the icon from this program first to allow overriding the
     // standard icons (although why one would want to do it considering that
     // we already have wxApp::GetStdIcon() is unclear)
@@ -635,13 +637,15 @@ bool wxICOResourceHandler::LoadIcon(wxIcon *icon,
         else // We may still succeed in loading the standard icon.
         {
             const auto& entry = stdIcons[nStdIcon];
-            hicon = (HICON)::LoadImage((HINSTANCE)nullptr, entry.id, IMAGE_ICON,
-                                       desiredWidth, desiredHeight,
-                                       LR_DEFAULTSIZE | LR_DEFAULTCOLOR);
+            hicon = ::LoadIcon((HINSTANCE)nullptr, entry.id);
             if ( !hicon )
             {
-                wxLogLastError(wxString::Format("LoadImage(%s)", entry.name));
+                wxLogLastError(wxString::Format("LoadIcon(%s)", entry.name));
             }
+
+            // LoadIcon() may scale the icon in high DPI, so get its actual
+            // size below.
+            iconMayBeScaled = true;
         }
     }
 
@@ -649,17 +653,22 @@ bool wxICOResourceHandler::LoadIcon(wxIcon *icon,
         return false;
 
     wxSize size;
+    double scale = 1.0;
     if ( hasSize )
     {
         size.x = desiredWidth;
         size.y = desiredHeight;
     }
-    else
+    else if ( iconMayBeScaled )
     {
+        // LoadIcon() returns icons of scaled size, so we must use the correct
+        // scaling factor of them.
         size = wxGetHiconSize(hicon);
+        if ( const wxWindow* win = wxApp::GetMainTopWindow() )
+            scale = win->GetDPIScaleFactor();
     }
 
-    return icon->InitFromHICON((WXHICON)hicon, size.x, size.y);
+    return icon->InitFromHICON((WXHICON)hicon, size.x, size.y, scale);
 }
 
 #if wxUSE_PNG_RESOURCE_HANDLER